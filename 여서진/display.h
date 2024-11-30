cb79a94968eb40d21046c80c84d549310ed29e9b
custard tart <71209588+tarte-aux-oeufs@users.noreply.github.com>
2024-11-30 21:51:37 +0900

    Delete 여서진 directory

--- 여서진/display.h
+++
@@ -1,8 +0,0 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
-
-void draw_main_menu();       // 메뉴 출력
-void draw_lists();           // 리마인더와 습관 목록 출력
-
-#endif
-
--- 여서진/event.h
+++
@@ -1,8 +0,0 @@
-#ifndef EVENT_H
-#define EVENT_H
-
-// event_submenu 함수 선언
-void event_submenu();
-
-#endif
-
