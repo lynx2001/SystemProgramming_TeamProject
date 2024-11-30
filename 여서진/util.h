36d466da72afd4f673551cafa45b4ec4e9e4976e
custard tart <71209588+tarte-aux-oeufs@users.noreply.github.com>
2024-11-30 21:13:15 +0900

    Update util.h

--- 여서진/util.h
+++ 여서진/util.h
@@ -1,14 +1,19 @@
-// event_io.h
+// util.h
 #ifndef UTIL_H
 #define UTIL_H
 
-#include "global.h"
 #include <stdbool.h>
 #include <ncurses.h>
 #include <string.h>
 
+extern bool popup_message_called;
+
+//유효성 테스트용 임시
 bool validateDate(int year, int month, int day);
 bool validateTime(int hour, int minute);
+
 void popup_message(const char *message);
+void handle_resize(int sig);
+int get_input(const char *prompt, char *buffer, int size);
 
 #endif
