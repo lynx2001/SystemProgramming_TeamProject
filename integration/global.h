2c972a7c2618b5b055a46cf1a0c29745eadc8020
이시욱 <rnl@isiug-ui-MacBookAir.local>
2024-12-04 19:41:27 +0900

    Event 구조체 최종본

--- integration/global.h
+++ integration/global.h
@@ -23,14 +23,21 @@ typedef struct {
     char title[50];
     Time date_start;
     Time date_end;
-    double quantity;    // 총 분량 / 총 일수 = 하루당 분량
     int importance;     // 0 ~ 5 사이 정수
-    char details[100];  // 상세 기록
-
+    double quantity;    // 총 분량 / 총 일수 = 하루당 분량
+    
     // 아래는 계산되는 값
     double interval;    // 마감일 - 시작일
     double Dday;        // 마감일 - 현재일(now)
     double weight;      // 가중치 식에 따라 계산 (지역변수 importance를 사용해서 계산) >> default : -1
+
+    char details[100];  // 상세 기록
+
+    /* 계산 되는 값
+    interval
+    Dday
+    weight
+    */
 
     /* default 정리 (일반 일정에 필요 없는 값)  >> 다 기본값 -1
     quantity
@@ -38,6 +45,12 @@ typedef struct {
     interval
     weight
     */
+
+   /* 저장 필드 값 변경 사항 기록 */
+   /*
+   기존 : id|title|date_start|date_end|interval|Dday|weight|quantity|details
+   변경 : id|title|date_start|date_end|importance|quantity|interval|Dday|weight|details
+   */
 } Event;
 
 extern Event events[MAX_EVENTS];
