fa221e73a21ef02449d7078092944a7400b8ea8a
custard tart <71209588+tarte-aux-oeufs@users.noreply.github.com>
2024-12-10 16:09:34 +0900

    새로 추가된 함수 선언

--- integration/util.h
+++ integration/util.h
@@ -11,12 +11,19 @@ extern bool popup_message_called;
 //유효성 테스트용 임시
 bool validateDate(int year, int month, int day);
 bool validateTime(int hour, int minute);
+bool validate_date_wrapper(const char *input);
+bool validate_time_wrapper(const char *input);
+bool validate_reminder(const char *input);
+bool validate_weight(const char *input);
+bool validate_quantity(const char *input);
 //validTest
 
-
 void popup_message(const char *message);
+int popup_confirmation(const char *message);
+void reset_input_position();
 void handle_resize(int sig);
-int get_input(const char *prompt, char *buffer, int size);
+int get_input(char *buffer, int size);
+int process_user_input(UIScreen *screen);
 
 //Sorting
 int compareByWeight(const void* a, const void* b);
